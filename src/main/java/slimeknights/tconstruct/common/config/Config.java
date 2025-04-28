e7017c4c6f9a5c4d57acf62a47ffeb1e6bb88980
Me <135455255+IcarussOne@users.noreply.github.com>
2025-04-28 13:28:33 -0500

    Add Fish's Undead Rising support for beheading

--- src/main/java/slimeknights/tconstruct/common/config/Config.java
+++ src/main/java/slimeknights/tconstruct/common/config/Config.java
@@ -85,7 +85,10 @@ public final class Config {
           "minecraft:zombie;true;minecraft:skull:2",
           "minecraft:player;false;minecraft:skull:3",
           "minecraft:creeper;true;minecraft:skull:4",
-          "minecraft:ender_dragon;true;minecraft:skull:5"
+          "minecraft:ender_dragon;true;minecraft:skull:5",
+          "mod_lavacow:boneworm;true;minecraft:skull:0",
+          "mod_lavacow:skeletonking;false;minecraft:skull:0",
+          "mod_lavacow:scarecrow;false;mod_lavacow:scarecrowhead_common"
   };
   public static String[] entityMelting = {
           "minecraft:villager_golem;true;iron;18",
